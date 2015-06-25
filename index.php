6c88fd560b83a63f08dc5619dfd5bb8ab3539777
dan-stachlewski <dan.stachlewski@gmail.com>
2015-06-25 21:50:44 +1000

    Created the Login Form on the index.php file.

--- index.php
+++ index.php
@@ -6,15 +6,38 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Title</title>
+    <title>Login</title>
 
     <link rel="stylesheet" href="css/style.css">
     <link rel="author" href="humans.txt">
   </head>
   <body>
   <section>
-    <h1>{Enter Page Title}</h1>
-    <p>{Enter information about the page...}</p>
+    <h1>Simple Login System</h1>
+    <p>Please enter your Email address or Username and Password to Login.</p>
+
+    <form action="" method="POST">
+      <p>
+        <label for="">Email</label>
+        <input type="text" name="Email" value="">
+      </p>
+
+      <p>
+        <label for="">Username</label>
+        <input type="text" name="UserName" value="">
+      </p>
+
+      <p>
+        <label for="">Password</label>
+        <input type="password" name="Password" value="">
+      </p>
+
+      <p>
+        <input type="submit" name="Login" value="Login">
+      </p>
+    </form>
+
+
   </section>
   <script src="js/main.js"></script>
   </body>
