b2e5d6600bfa1197b9032faedf784a7c8eab894e
dan-stachlewski <dan.stachlewski@gmail.com>
2015-06-25 22:02:46 +1000

    Created a DIR for includes in the website. Database connection.php lives here.

---
+++ includes/connection.php
@@ -0,0 +1,26 @@
+<?php
+
+/*********************************************
+ * CREATE A CONNECTION TO THE DATABASE
+ *********************************************
+ *
+ * DATABASE NAME: world_pic
+ * TABLE NAME: country
+ * TABLE NAME: location
+ * TABLE NAME: photographer
+ * TABLE NAME: picture
+ * TABLE NAME: public_comment
+ * TABLE NAME: public_users
+ * TABLE NAME: worldregion
+ * USERNAME: root
+ * PASSWORD: password
+ *
+ * CREATING CONNECTION VIA PDO
+ *
+ */
+
+
+
+
+
+
