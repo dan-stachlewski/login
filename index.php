ec5d9f4d564e612b56bfcdb92e9688c7b1b0ba6d
dan-stachlewski <dan.stachlewski@gmail.com>
2015-06-25 21:45:40 +1000

    Created a Bare Bones HTML5 index.php template to use as my basic Login Form

---
+++ index.php
@@ -0,0 +1,22 @@
+<!-- HTML5 index.php START -->
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Title</title>
+
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="author" href="humans.txt">
+  </head>
+  <body>
+  <section>
+    <h1>{Enter Page Title}</h1>
+    <p>{Enter information about the page...}</p>
+  </section>
+  <script src="js/main.js"></script>
+  </body>
+</html>
+<!-- HTML5 index.php END -->
