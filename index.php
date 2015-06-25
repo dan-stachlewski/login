66b98e3028070a1537631a830b293b1f0cb9d3e6
dan-stachlewski <dan.stachlewski@gmail.com>
2015-06-25 22:29:39 +1000

    Set up PHP Validation Steps when User tries to Login.

--- index.php
+++ index.php
@@ -1,3 +1,40 @@
+<?php 
+
+//RETRIEVE DATABASE CONNECTION INFORMATION
+include('includes/connection.php');
+
+//DECLARING VARIABLES AS EMPTY
+$Email = "";
+$UserName = "";
+$Password = "";
+
+//TEST WHETHER THE VARIABLES HAVE BEEN ASSIGNED DATA [ *** REMOVE @ RUNTIME *** ]
+echo "<pre> POST SUPERGLOBAL ";
+print_r($_POST);
+echo "</pre>";
+
+//CHECK IF LOGIN BUTTON PRESSED CARRY OUT FOLLOWING FUNCTIONS
+if (isset($_POST['submit'])) {
+
+//CARRY OUT DATA VALIDATION
+//CHECK IF EMAIL FORMAT HAD BEEN ENTERED
+//CHECK IF USERNAME HAD BEEN ENTERED & NOT EMPTY
+//CHECK IF PASSWORD HAD BEEN ENTERED & NOT EMPTY
+
+//CHECK IF EMAIL & PASSWORD HAVE BEEN ENTERED - B/C OF COMBO CHOICE ONLY 2/3 FIELDS REQUIRED
+
+//CHECK IF USERNAME & PASSWORD HAVE BEEN ENTERED - B/C OF COMBO CHOICE ONLY 2/3 FIELDS REQUIRED
+
+//ASSIGNING POST DATA TO VARIABLES FOR MANIPULATION - SANITISATION BEFORE COMPARING CREDENTIALS TO INFORMATION ON DATABASE[world_pic] , TABLE[public_users]
+$Email = $_POST['Email'];
+$UserName = $_POST['UserName'];
+$Password = $_POST['Password'];
+
+}
+
+?>
+
+
 <!-- HTML5 index.php START -->
 <!DOCTYPE html>
 <html lang="en">
@@ -42,4 +79,4 @@
   <script src="js/main.js"></script>
   </body>
 </html>
-<!-- HTML5 index.php END -->
+<!-- HTML5 index.php END Updated the index.php file - now had the basic Login Form.-->
